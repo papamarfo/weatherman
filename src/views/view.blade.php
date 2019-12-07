abaa8f3621fea816438d6182672e76abfd29ff6c
Benjamin Manford <benjaminmanford@gmail.com>
2019-12-07 21:48:43 +0800

    Change variable name from $data to $city

--- src/views/view.blade.php
+++ src/views/view.blade.php
@@ -1,19 +1,19 @@
 <div class="card-body">
-    <h2>{{ $data->name }} Weather Status</h2>
+    <h2>{{ $city->name }} Weather Status</h2>
     <div class="time">
         <div>{{ date("l g:i a", time()) }}</div>
         <div>{{ date("jS F, Y", time()) }}</div>
-        <div>{{ ucwords($data->weather[0]->description) }}</div>
+        <div>{{ ucwords($city->weather[0]->description) }}</div>
     </div>
     <div class="weather-forecast">
-        <img src="http://openweathermap.org/img/w/{{ $data->weather[0]->icon }}.png"
-            class="weather-icon" /> <?php echo $data->main->temp_max; ?>°C
+        <img src="http://openweathermap.org/img/w/{{ $city->weather[0]->icon }}.png"
+            class="weather-icon" /> <?php echo $city->main->temp_max; ?>°C
         <span class="min-temperature">
-            {{ $data->main->temp_min }}°C
+            {{ $city->main->temp_min }}°C
         </span>
     </div>
     <div class="time">
-        <div>Humidity: {{ $data->main->humidity }} %</div>
-        <div>Wind: {{ $data->wind->speed }} km/h</div>
+        <div>Humidity: {{ $city->main->humidity }} %</div>
+        <div>Wind: {{ $city->wind->speed }} km/h</div>
     </div>
 </div>
