18b74a5b32ecafef0d0697441b2d34df2b82e45f
Benjamin Manford <benjaminmanford@gmail.com>
2019-12-15 23:45:07 +0800

    Format the Weatherman.php class

--- src/Weatherman.php
+++ src/Weatherman.php
@@ -7,51 +7,51 @@ use Exception;
 
 class Weatherman
 {
-	/**
-	 * Create a new Constructor
-	 */
-	public function __construct()
-	{
-		$this->client = new Client();
-		$this->app_id = config('weatherman.app_id');
-		$this->base_url = config('weatherman.base_url');
-	}
-
-	/**
-	 * Get weather by city
-	 *
-	 * @param $city
-	 */
+    /**
+     * Create a new Constructor
+     */
+    public function __construct()
+    {
+        $this->client   = new Client();
+        $this->app_id   = config('weatherman.app_id');
+        $this->base_url = config('weatherman.base_url');
+    }
+    
+    /**
+     * Get weather by city
+     *
+     * @param $city
+     */
     public function city($city)
     {
-    	try
-		{
-			$response = $this->client->request(
-				'GET', $this->base_url.'?q='.$city.'&appid='.$this->app_id
-			);
-		}
-		catch (Exception $e)
-		{
-			logger()->error($e->getMessage());
-
+        try
+        {
+            $response = $this->client->request(
+                'GET', $this->base_url . '?q=' . $city . '&appid=' . $this->app_id
+            );
+        }
+        catch (Exception $e)
+        {
+            logger()->error($e->getMessage());
+            
             return [];
-		}
-
-		return $this->responseHandler($response->getBody()->getContents());
+        }
+        
+        return $this->responseHandler($response->getBody()->getContents());
     }
+    
+    /**
+     * Response handler
+     *
+     * @param $response
+     */
+    public function responseHandler($response)
+    {
+        if ($response)
+        {
+            return json_decode($response);
+        }
 
-	/**
-	 * Response handler
-	 *
-	 * @param $response
-	 */
-	public function responseHandler($response)
-	{
-		if ($response)
-		{
-			return json_decode($response);
-		}
-		
-		return [];
-	}
+        return [];
+    }
 }
